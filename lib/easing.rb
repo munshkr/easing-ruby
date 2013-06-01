a6a64462a9c6e3bd2d4eeb09f7fab2e5da518aa9
Damián Silvani <munshkr@gmail.com>
2013-06-01 09:58:53 -0300

    Add some easing functions and test them (a bit)

--- lib/easing.rb
+++ lib/easing.rb
@@ -1,5 +1,35 @@
 require "easing/version"
 
 module Easing
-  # Your code goes here...
+  extend self
+
+  def linear_tween(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    c * t / d + b
+  end
+
+  def ease_in_out_quad(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    t /= d / 2
+    return c / 2*t*t + b if (t < 1)
+    t -= 1
+    return -c/2 * (t*(t-2) - 1) + b
+  end
+
+  def ease_in_out_expo(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return b if t == 0
+    return b + c if t == d
+    return (c/2) * 2**(10 * (t-1)) + b if ((t /= d/2) < 1)
+    return (c/2) * (-2**(-10 * t-=1) + 2) + b
+  end
+
+  def ease_out_expo(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return (t==d) ? b+c : c * (-2**(-10 * t/d) + 1) + b
+  end
 end
--- test/easing_test.rb
+++ test/easing_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
+require 'easing'
 
 describe Easing do
-  it 'should be true' do
-    assert true
+  describe 'linear_tween' do
+    it { assert_equal 10, Easing.linear_tween(10, 0, 100, 100) }
+    it { assert_equal 90, Easing.linear_tween(90, 0, 100, 100) }
+  end
+
+  describe 'ease_in_out_quad' do
+    it { assert_equal 2.0, Easing.ease_in_out_quad(10, 0, 100, 100) }
+    it { assert_equal 98.0, Easing.ease_in_out_quad(90, 0, 100, 100) }
+  end
+
+  describe 'ease_in_out_expo' do
+    it { assert_equal 0.1953125, Easing.ease_in_out_expo(10, 0, 100, 100) }
+    it { assert_equal 99.8046875, Easing.ease_in_out_expo(90, 0, 100, 100) }
   end
 end
