7c688a3d81987ca5c62d982d6465a60d6b49fa9f
Damián Silvani <munshkr@gmail.com>
2013-06-01 15:34:55 -0300

    Define more easing functions
    Only "elastic", "back" and "bounce" are missing

--- lib/easing.rb
+++ lib/easing.rb
@@ -9,6 +9,18 @@ module Easing
     c * t / d + b
   end
 
+  def ease_in_quad(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c*(t/=d)*t + b;
+  end
+
+  def ease_out_quad(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return -c *(t/=d)*(t-2) + b;
+  end
+
   def ease_in_out_quad(t, b, c, d)
     t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
 
@@ -16,6 +28,93 @@ module Easing
     return c / 2*t*t + b if (t < 1)
     t -= 1
     return -c/2 * (t*(t-2) - 1) + b
+  end
+
+  def ease_in_cubic(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c*(t/=d)*t*t + b
+  end
+
+  def ease_out_cubic(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c*((t=t/d-1)*t*t + 1) + b
+  end
+
+  def ease_in_out_cubic(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c/2*t*t*t + b if ((t/=d/2) < 1)
+    return c/2*((t-=2)*t*t + 2) + b
+  end
+
+  def ease_in_quart(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c*(t/=d)*t*t*t + b
+  end
+
+  def ease_out_quart(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return -c * ((t=t/d-1)*t*t*t - 1) + b
+  end
+
+  def ease_in_out_quart(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c/2*t*t*t*t + b if ((t/=d/2) < 1)
+    return -c/2 * ((t-=2)*t*t*t - 2) + b
+  end
+
+  def ease_in_quint(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c*(t/=d)*t*t*t*t + b
+  end
+
+  def ease_out_quint(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c*((t=t/d-1)*t*t*t*t + 1) + b
+  end
+
+  def ease_in_out_quint(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c/2*t*t*t*t*t + b if ((t/=d/2) < 1)
+    return c/2*((t-=2)*t*t*t*t + 2) + b
+  end
+
+  def ease_in_sine(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return -c * Math.cos(t/d * (Math::PI/2)) + c + b
+  end
+
+  def ease_out_sine(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c * Math.sin(t/d * (Math::PI/2)) + b
+  end
+
+  def ease_in_out_sine(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return -c/2 * (Math.cos(Math::PI*t/d) - 1) + b
+  end
+
+  def ease_in_expo(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return (t==0) ? b : c * (2 ** (10 * (t/d - 1))) + b
+  end
+
+  def ease_out_expo(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return (t==d) ? b+c : c * (-2**(-10 * t/d) + 1) + b
   end
 
   def ease_in_out_expo(t, b, c, d)
@@ -27,9 +126,22 @@ module Easing
     return (c/2) * (-2**(-10 * t-=1) + 2) + b
   end
 
-  def ease_out_expo(t, b, c, d)
+  def ease_in_circ(t, b, c, d)
     t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
 
-    return (t==d) ? b+c : c * (-2**(-10 * t/d) + 1) + b
+    return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b
+  end
+
+  def ease_out_circ(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return c * Math.sqrt(1 - (t=t/d-1)*t) + b
+  end
+
+  def ease_in_out_circ(t, b, c, d)
+    t = t.to_f; b = b.to_f; c = c.to_f; d = d.to_f
+
+    return -c/2 * (Math.sqrt(1 - t*t) - 1) + b if ((t/=d/2) < 1)
+    return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b
   end
 end
